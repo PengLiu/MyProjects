11f131eb50c4dbae0834867bfecbe170471604ff
Liu Peng <pengliu1980@gmail.com>
2011-03-24 11:43:22 +0800

    Init commit

---
+++ TankWar/TankWar/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  TankWar
+//
+//  Created by Ammen on 11-3-24.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ TankWar/TankWar/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TankWar
+//
+//  Created by Ammen on 11-3-24.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
